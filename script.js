7bccb84ca97f859af13ad120472113696d6c2087
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-03 15:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+$(document).ready(function(){
+            $('.slider').slick({
+                dots: true,
+                infinite: true,
+                speed: 500,
+                fade: true,
+                cssEase: 'linear'
+            });
+
+            $('#imageModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget);
+                var slideIndex = button.data('slide-to');
+                $('.slider').slick('slickGoTo', parseInt(slideIndex));
+            });
+        });
